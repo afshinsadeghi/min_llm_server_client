d7bbb50c035b29248c01a900ae3d0619e3e9bc3a
Afshin Sadeghi <afshin.sadeghi@iais.fraunhofer.de>
2025-09-15 12:32:47 +0200

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='min_llm_server_client',
-    version='0.1.0',
+    version='0.3.7.1',
     author='Afshin Sadeghi',
     author_email='afshin.sadeghi@example.com',
-    description='A minimal REST API server and client for local LLM inference.',
+    description='A minimal API server and client for local LLM inference.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
